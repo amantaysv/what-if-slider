c5e7cb11ef6139438a568ad00222bf97821adcab
Amantay <amantay.sv@gmail.com>
2021-10-03 09:33:58 +0600

    reorganized

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,7 @@ const upBtn = document.querySelector('.up-button')
 const downBtn = document.querySelector('.down-button')
 const sidebar = document.querySelector('.sidebar')
 const mainSlide = document.querySelector('.main-slide')
+
 const slidesCount = mainSlide.querySelectorAll('div').length
 const container = document.querySelector('.container')
 
@@ -12,19 +13,20 @@ sidebar.style.top = `-${(slidesCount - 1) * 100}vh`
 upBtn.addEventListener('click', () => {
   changeSlide('up')
 })
+
 downBtn.addEventListener('click', () => {
   changeSlide('down')
 })
 
 function changeSlide(direction) {
-  if(direction === 'up') {
+  if (direction === 'up') {
     activeSlideIndex++
-    if(activeSlideIndex === slidesCount) {
+    if (activeSlideIndex === slidesCount) {
       activeSlideIndex = 0
     }
-  } else if(direction === 'down') {
+  } else if (direction === 'down') {
     activeSlideIndex--
-    if(activeSlideIndex < 0) {
+    if (activeSlideIndex < 0) {
       activeSlideIndex = slidesCount - 1
     }
   }
